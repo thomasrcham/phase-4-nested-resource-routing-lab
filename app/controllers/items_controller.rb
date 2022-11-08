2364879b27441b6b6164597fa520ae23b4c65a52
thomasrcham <guseldorph@gmail.com>
2022-11-08 13:29:48 -0600

    commit tree 08bf61d231d310310c757c9dd780601955103556

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,27 @@
 class ItemsController < ApplicationController
-
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item =
+      user.items.create(
+        name: params[:name],
+        description: params[:description],
+        price: params[:price]
+      )
+    render json: item, status: :created
+  end
 end
